f6a0e84919957b24d8145f494aca41ec2a29b3a6
Anjali-st <anjali90908080@gmail.com>
2023-09-13 20:35:32 +0530

    Validation using MetaData

---
+++ Data/School1EntitiesDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Phase3Section2._20.Data
+{
+    public class School1EntitiesDbContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public School1EntitiesDbContext() : base("name=School1EntitiesDbContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Phase3Section2._20.Student> Students { get; set; }
+    }
+}
